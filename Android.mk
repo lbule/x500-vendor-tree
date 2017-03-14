7e9062f2ca126a99d2ddcf0e246eb3c956f5049e
Bule <586921@gmail.com>
2017-03-14 12:08:32 +0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -23,3 +23,11 @@ LOCAL_MULTILIB = 32
 LOCAL_SRC_FILES_32 = proprietary/lib/libdpframework.so
 include $(BUILD_PREBUILT)
 
+LOCAL_PATH := $(call my-dir)
+ifeq ($(TARGET_DEVICE),X500)
+# Making symlinks in libMcGatekeeper
+$(shell ln -sf libMcGatekeeper.so $(TARGET_OUT)/lib64/hw/gatekeeper.mt6795.so; \
+	ln -sf libMcGatekeeper.so $(TARGET_OUT)/lib64/hw/gatekeeper.x500.so; \
+	ln -sf libMcGatekeeper.so $(TARGET_OUT)/lib/hw/gatekeeper.mt6795.so; \
+	ln -sf libMcGatekeeper.so $(TARGET_OUT)/lib/hw/gatekeeper.x500.so; )
+endif
